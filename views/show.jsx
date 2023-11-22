f44fd24db4ffb9d00086f265894162c878b4cd48
yordanos woldeyes <107896300+yordanosW@users.noreply.github.com>
2023-11-21 20:11:40 -0800

    sending index and show data

---
+++ views/show.jsx
@@ -0,0 +1,38 @@
+const React = require('react')
+const Default = require('./layouts/Default')
+
+function Show ({bread}) {
+  console.log(bread.name)
+    return (
+      <Default>
+        <h2>Show Page</h2>
+      </Default>
+    )
+}
+
+module.exports = Show
+function Show ({bread}) {
+    // Confirm we are getting our bread data in the terminal.
+    // console.log(bread.name)
+      return (
+        <Default>
+          <h2>Show Page</h2>
+          <h3>{bread.name}</h3>
+          <img src={bread.image} alt={bread.name} />
+        </Default>
+      )
+  }
+  <Default>
+  <h3>{bread.name}</h3>
+  <p>
+    and it
+    {
+      bread.hasGluten
+      ? <span> does </span>
+      : <span> does NOT </span>
+    }
+    have gluten.
+  </p>
+  <img src={bread.image} alt={bread.name} />
+  <li><a href="/breads">Go home</a></li>
+</Default>
