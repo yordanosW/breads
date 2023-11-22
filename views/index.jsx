ec30d7dcf0bf7b8d18ef4a2d853c436502a0e75f
yordanos woldeyes <107896300+yordanosW@users.noreply.github.com>
2023-11-21 20:33:26 -0800

    create and new routes complete

--- views/index.jsx
+++ views/index.jsx
@@ -17,6 +17,14 @@ function Index ({breads})  {
 }
 
         </ul>
+
+        <div className="newButton">
+  <a href="/breads/new"><button>Add a new bread</button></a>
+</div>
+<div className="backButton">
+  <a href="/breads"><button>Go back to the index</button></a>
+</div>
+
       </Default>
       
     )
